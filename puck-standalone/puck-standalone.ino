f46b0c709c0bc1c2ca6d195a0d0aee6b5af4e0d6
ericheep <ericheep@gmail.com>
2017-04-22 14:20:20 -0700

    erased some old stuff, added basic timer functions

--- puck-standalone/puck-standalone.ino
+++ puck-standalone/puck-standalone.ino
@@ -119,7 +119,6 @@ HSV randomHSV() {
   return  randomHSV(0, 360, .4, 1, .4, 1);
 }
 
-
 HSV randomHSV(int minH, int maxH, float minS, float maxS, float minV, float maxV) {
   int h = random(minH, maxH);
   float s = (random(int(minS * 1000), int(maxS * 1000)) / 1000.0);
@@ -129,271 +128,35 @@ HSV randomHSV(int minH, int maxH, float minS, float maxS, float minV, float maxV
   };
 }
 
-void gradientShifts(float add, float seconds) {
-  float milliwait = 10;
-  float inc = 0;
-  int iterations = seconds / (milliwait * 0.001);
-
-  for (int k = 0; k < iterations; k++) {
-    inc = inc + add;
-
-    if (inc > 360) {
-      inc = inc - 360;
-    }
-    Tlc.clear();
-
-    for (int i = 0; i < NUM_LEDS; i++) {
-      HSV hsv = {
-        240 + i * 5, 1, 1
-      };
-      setColor(i, hsv);
-    }
-
-    Tlc.update();
-    delay(10);
-  }
+float incrementTime(float milliseconds) { 
+  return (2.0 * PI)/milliseconds;
 }
 
-void walkDrop() {
-  int current_leds[16];
-  HSV current_hsvs[16];
-  long current_ttls[16];
+const float SECOND = 100;
+const float MINUTE = 60 * SECOND;
+const float HOUR = 60 * MINUTE;
+const float DAY = 24 * MINUTE;
 
-  int max_ttl = 2000;
-  int min_ttl = 2000;
-  float minS = .2;
-  float maxS = 1;
-  float minV = .4;
-  float maxV = .6;
+float cycle = 0.0;
+float color = 0.0;
+float range = 360;
+float offset = 0.0;
 
-
-  for (int i = 0; i < 16; i++) {
-    current_leds[i] = random(0, NUM_LEDS);
-    current_hsvs[i] = randomHSV(0, 360, minS, maxS, minV, maxV);
-    current_ttls[i] = random(min_ttl, max_ttl);
-  }
-
-  Tlc.clear();
-  for (int t = 0; t < 3000; t++) {
-    Tlc.clear();
-    for (int i = 0; i < 16; i++) {
-      int change = 0;
-      int glow = 0;
-
-      if (glow) {
-        current_hsvs[i].v += .01;
-        if (current_hsvs[i].v > 1) {
-          change = 1;
-        }
-      }
-      else {
-        current_hsvs[i].v -= .01;
-        if (current_hsvs[i].v < 0) {
-          change = 1;
-        }
-      }
-      current_ttls[i] -= 1;
-      if (current_ttls[i] < 0) {
-        change = 1;
-      }
-
-      if (change) {
-        current_leds[i] = random(0, NUM_LEDS);
-        current_hsvs[i] = randomHSV(0, 360, minS, maxS, minV, maxV);
-        current_ttls[i] = random(min_ttl, max_ttl);
-      }
-      setColor(current_leds[i], current_hsvs[i]);
-
-    }
-    Tlc.update();
-    delay(10);
-  }
-}
-
-void oneColor(int hue, float s, float v) {
-  Tlc.clear();
-  for (int i = 0; i < NUM_LEDS; i++) {
-    HSV hsv = {
-      hue, s, v
-    };
-    setColor(i, hsv);
-  }
-  Tlc.update();
-  //fadeInOut(random(0, 16), 300, 3000);
-}
-
-void oneOffColor(int hue, float s, float v) {
-  Tlc.clear();
-  for (int i = 0; i < NUM_LEDS; i++) {
-    HSV hsv = {
-      hue, s, v
-    };
-    setColor(i, hsv);
-  }
-  HSV hsv = {
-    0, s, v
-  };
-  setColor(random(0, 16), hsv);
-
-  HSV bsv = {
-    0, 0, 0
-  };
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-  setColor(random(0, 16), bsv);
-
-  Tlc.update();
-}
-
-/*
-  void fadingRows(int hue) {
-  int perRow[3] {
-    0, 6, 11
-  };
-
-  for (int i = 0; i < 3; i++) {
-    for (int i = 0; i < 5; i++) {
-      HSV hsv = {
-        hue, 1, j
-      };
-      setColor(i, hsv);
-    }
-    Tlc.update();
-    delay(30);
-  }
-
-  for (float j = 0.0; j < 1.0; j = j + 0.01) {
-    for (int i = 0; i < 5; i++) {
-      HSV hsv = {
-        hue, 1, j
-      };
-      setColor(i, hsv);
-    }
-    Tlc.update();
-    delay(30);
-  }
-  for (float j = 1.0; j <= 0.0; j = j - 0.01) {
-    for (int i = 0; i < 5; i++) {
-      HSV hsv = {
-        hue, 1, j
-      };
-      setColor(i, hsv);
-    }
-    Tlc.update();
-    delay(30);
-  }
-  Tlc.clear();
-  }
-*/
-
-void fadeInOut(int led, int hue, float milliseconds) {
-  //Tlc.clear();
-  float delaySpeed = milliseconds / 200.0;
-
-  for (float i = 0.0; i < 1.0; i = i + 0.01) {
-    HSV hsv = {
-      hue, 1, i
-    };
-    setColor(led, hsv);
-    Tlc.update();
-    delay(delaySpeed);
-  }
-  for (float i = 1.0; i >= 0.0; i = i - 0.01) {
-    HSV hsv = {
-      hue, 1, i
-    };
-    setColor(led, hsv);
-    Tlc.update();
-    delay(delaySpeed);
-  }
-}
-
-void fadingRows(int hue, float milliseconds) {
-  //Tlc.clear();
-  float delaySpeed = milliseconds / 200.0;
-
-  int row[] = {0, 0, 0, 0, 0, 0};
-  int rowOne[] = { 0, 1, 2, 3, 4, 0 };
-  int rowTwo[] = { 5, 6, 7, 8, 9, 10 };
-  int rowThree[] = { 11, 12, 13, 14, 15, 14};
-
-  for (int k = 0; k < 4; k++) {
-    for (int i = 0; i < 6; i++) {
-      if (k == 0) {
-        row[i] = rowOne[i];
-      }
-      if (k == 1) {
-        row[i] = rowTwo[i];
-      }
-      if (k == 2) {
-        row[i] = rowThree[i];
-      }
-      if (k == 3) {
-        row[i] = rowTwo[i];
-      }
-    }
-    for (float i = 0.25; i < 1.0; i = i + 0.01) {
-      for (int j = 0; j < 6; j++) {
-        HSV hsv = {
-          hue, 1, i
-        };
-        setColor(row[j], hsv);
-      }
-      Tlc.update();
-      delay(delaySpeed);
-    }
-    for (float i = 1.0; i >= 0.25; i = i - 0.01) {
-      for (int j = 0; j < 6; j++) {
-        HSV hsv = {
-          hue, 1, i
-        };
-        setColor(row[j], hsv);
-      }
-      Tlc.update();
-      delay(delaySpeed);
-    }
-  }
-}
-
+float incrementSize = incrementTime(DAY);
 
 void loop() {
-  // walkDrop();
-  // cyclingGradients();
-  // gradientShifts(0.2, 30);
-  // oneColor(70, 1.0, 1.0);
-
-  inc = fmod((inc + 0.02), PI);
-  sinInc = sin(inc) * 15;
-
-  brightInc = fmod((brightInc + 0.005), PI);
-  sinBright = (sin(brightInc)) * 1;
+  cycle = fmod(cycle + incrementSize, 2.0 * PI);
+  color = offset + range * (sin(cycle) + 1.0) * 0.5;
   
   for (int i = 0; i < 16; i++) {
     HSV hsv = {
-      30, 1, sinBright * sinBright * sinBright
+      color, 1.0, 0.005
     };
     setColor(i, hsv);
     Tlc.update();
   }
 
-  Serial.println(sinBright);
+  Serial.println(color);
   delay(10);
-  // fadeInOut(i, 30, 200);
-  // fadingRows(30, 5000);
 }
 
